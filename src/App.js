23df7bd4d9ecb651f602185687bab66b3a3e0183
wer <wr@gtr.re>
2024-07-23 11:41:18 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import CountClass from './Components/ClassComponents';
+import CountFunct from './Components/FunctionComponents';
 
 function App() {
+  const btnRef =  useRef(null)
+  const [isCount, setIsCount] = useState(true)
+
+  useEffect(()=>{
+    console.dir(btnRef.current)
+  },[])
 
   return (
     <div className="App">
+        <button ref={btnRef} onClick={()=>{setIsCount(!isCount)}}>{isCount?"Tat ":"Bat "}</button>
+        {
+          isCount && 
+          // <CountClass/>
+          <CountFunct></CountFunct>
+        }
     </div>
   );
 }
